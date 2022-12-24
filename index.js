08384673f16ff4996aec911070619437c497dc17
JayPatel202002 <107167842+JayPatel202002@users.noreply.github.com>
2022-12-23 20:20:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const path = require('path');
 const express = require('express');
-const dotenv = require('dotenv').config();
+const dotenv = require('dotenv').config()
 
-const port = 6500;
-const app = express();
+const port = 6500
+const app = express()
 
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname,'public')))
 
-app.use('/openai', require('./routes/openaiRoutes'));
+app.use('/openai', require('./routes/openaiRoutes'))
 
-app.listen(port, ()=> console.log(`Server is running on ${port}`));
+app.listen(port, ()=> console.log(`Server is running on ${port}`))
